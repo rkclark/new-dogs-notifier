08e688c221e34142ec5ea326025db51d0e27bc19
Rick Clark <rkingsleyclark@gmail.com>
2020-07-28 11:43:02 +0100

    Get all border collies from first two pages

--- index.js
+++ index.js
@@ -6,10 +6,30 @@ const options = {
   includeNodeLocations: true,
 };
 
-JSDOM.fromURL(
-  'https://www.dogstrust.org.uk/rehoming/dogs/filters/~269~~~~n~~/page/2',
+const getDogsTrustDogs = async (filterId, page) => {
+  const dom = await JSDOM.fromURL(
+    `https://www.dogstrust.org.uk/rehoming/dogs/filters/~${filterId}~~~~n~~/page/${page}`,
+    options,
+  );
+  const dogs = [...dom.window.document.querySelectorAll("a[id*='lnkDog']")].map(
+    (node) => {
+      return {
+        link: node.href,
+        name: node.querySelector('h3').textContent.replace('\n', '').trim(),
+      };
+    },
+  );
 
-  options,
-).then((dom) => {
-  console.log(dom.serialize());
-});
+  return dogs;
+};
+
+const getDogs = async () => {
+  let dogs = await Promise.all([
+    getDogsTrustDogs(269, 1),
+    getDogsTrustDogs(269, 2),
+  ]);
+  dogs = dogs.flat();
+  console.log(dogs, dogs.length);
+};
+
+getDogs();
